e2e6b9076728e010b2c8187c3e4eb2e4b01c6f14
node189 <node189@icloud.com>
2022-12-06 14:02:45 +0800

    NPC controller sciprt finished

--- Assets/Scripts/NPCCarController.cs
+++ Assets/Scripts/NPCCarController.cs
@@ -2,107 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NPCCarController : MonoBehaviour
 {
-    public float maxSpeed = 20f;
-
-    private float accleration = 20f;
-    private float decceleration = 15f;
-    private float carSpeed = 0;
-
-    private float horizontal;
-    private float vertical;
+    public enum NPCCarState
+    {
+        Move,
+        Stop,
+        End
+    }
 
     private Rigidbody carRigidbody;
-    private bool isGrounded = false;
-    private Vector3 startPos;
-    private Quaternion startRotate;
+
+    public NPCCarState state = NPCCarState.Stop;
+    public float speed = 10f;
+
+    public int maxTick = 200;
+    private int tick = 0;
 
     void OnTriggerStay(Collider other)
     {
-        if (other.transform.tag == "Floor")
+        if (other.transform.tag == "Floor" || other.transform.tag == "Destination")
         {
-            isGrounded = true;
+            state = NPCCarState.Move;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("test " + other.transform.tag);
+        if (other.transform.tag != "Floor" && other.transform.tag != "Destination")
+        {
+            state = NPCCarState.End;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.transform.tag == "Floor")
-        {
-            isGrounded = false;
-        }
+
     }
 
     void Start()
     {
-        startPos = transform.position;
-        startRotate = transform.rotation;
         carRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.R))
-        {
-            transform.position = startPos;
-            transform.rotation = startRotate;
-
-            carRigidbody.velocity = Vector3.zero;
-            carSpeed = 0;
-
-            isGrounded = false;
-
-            return;
-        }
-
-        // Car movement
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        if (!isGrounded)
-        {
-            vertical = 0;
-        }
-
-        // Four wheel turn aournd the x axis
-        transform.GetChild(1).Rotate(carSpeed * 100, 0, 0);
-        transform.GetChild(2).Rotate(carSpeed * 100, 0, 0);
-        transform.GetChild(3).Rotate(carSpeed * 100, 0, 0);
-        transform.GetChild(4).Rotate(carSpeed * 100, 0, 0);
     }
 
     void FixedUpdate()
     {
-        // Add gravity
-        float mass = GetComponent<Rigidbody>().mass;
-        GetComponent<Rigidbody>().AddForce(Vector3.down * mass);
-
-        // Cannot turn around around X and Z axis
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-
-        if (vertical > 0)
+        tick++;
+        if (tick == maxTick)
         {
-            if (carSpeed + accleration * Time.deltaTime <= maxSpeed)
-            {
-                carSpeed += accleration * Time.deltaTime;
-            }
-        }
-        else if (carSpeed > 0)
-        {
-            carSpeed -= decceleration * Time.deltaTime;
+            state = NPCCarState.End;
         }
 
-        if (carSpeed <= 0)
+        switch (state)
         {
-            carSpeed = 0;
-        }
-
-        transform.Rotate(0, horizontal * Time.deltaTime * 150, 0);
-        Debug.Log(isGrounded); // debug 
-        if (isGrounded)
-        {
-            carRigidbody.velocity = transform.forward * carSpeed;
+            case NPCCarState.Move:
+                carRigidbody.velocity = transform.forward * speed;
+                break;
+            case NPCCarState.End:
+                Destroy(gameObject);
+                break;
+            default:
+                break;
         }
     }
 }
